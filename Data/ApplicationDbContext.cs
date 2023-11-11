89afc28f2d8814f91cb119ae0fc4eee17021afba
GOLDER303 <szymonstepien@interia.pl>
2023-11-11 20:25:54 +0100

    Seed bingo table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TeachersBingoApi.Models;
 
 namespace TeachersBingoApi.Data;
@@ -11,4 +12,24 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Bingo> Bingos { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Bingo>().HasData(
+            new Bingo { Id = 1, Name = "Bingo 1" }
+        );
+
+        modelBuilder.Entity<Phrase>().HasData(
+            new Phrase() { Id = 1, Content = "Phrase 1", BingoId = 1 },
+            new Phrase() { Id = 2, Content = "Phrase 2", BingoId = 1 },
+            new Phrase() { Id = 3, Content = "Phrase 3", BingoId = 1 },
+            new Phrase() { Id = 4, Content = "Phrase 4", BingoId = 1 },
+            new Phrase() { Id = 5, Content = "Phrase 5", BingoId = 1 },
+            new Phrase() { Id = 6, Content = "Phrase 6", BingoId = 1 },
+            new Phrase() { Id = 7, Content = "Phrase 7", BingoId = 1 },
+            new Phrase() { Id = 8, Content = "Phrase 8", BingoId = 1 },
+            new Phrase() { Id = 9, Content = "Phrase 9", BingoId = 1 }
+        );
+    }
 }
---
+++ Migrations/20231111184524_SeedBingoTable.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TeachersBingoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBingoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Bingos",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Bingo 1" });
+
+            migrationBuilder.InsertData(
+                table: "Phrase",
+                columns: new[] { "Id", "BingoId", "Content" },
+                values: new object[,]
+                {
+                    { 1, 1, "Phrase 1" },
+                    { 2, 1, "Phrase 2" },
+                    { 3, 1, "Phrase 3" },
+                    { 4, 1, "Phrase 4" },
+                    { 5, 1, "Phrase 5" },
+                    { 6, 1, "Phrase 6" },
+                    { 7, 1, "Phrase 7" },
+                    { 8, 1, "Phrase 8" },
+                    { 9, 1, "Phrase 9" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Phrase",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Bingos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
