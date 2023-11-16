6c92e340581e7cef1be0d810c52e0eddd4c1bfa0
GOLDER303 <szymonstepien@interia.pl>
2023-11-16 20:00:46 +0100

    Add Newtonsoft json

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddScoped<IBingoService, BingoService>();
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IBingoGameService, BingoGameService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
