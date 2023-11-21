9a93de761708e5b75d7f235ac6836e2f0e78fc45
GOLDER303 <szymonstepien@interia.pl>
2023-11-21 18:43:45 +0100

    Fix cors config

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy
-                              .WithOrigins("http://http://localhost:5173")
+                              .WithOrigins("http://localhost:5173")
                               .AllowAnyHeader()
                               .AllowAnyMethod();
                       });
